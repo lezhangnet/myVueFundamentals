6c53c4d305d51b95a5bd7a2843055ff9f5f8ddc2
Le Zhang <zhale@amazon.com>
2021-02-07 23:50:47 -0800

    mini component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <MiniComponent msg="a passed in msg" />
+    <mini-component msg="also working?!" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import MiniComponent from './components/MiniComponent.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    MiniComponent,
   },
 };
 </script>
---
+++ src/components/MiniComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mini">
+    <h1>This is {{ msg }}</h1>
+    <p>
+      This is a minimal component.
+      Copy me as base of new components.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MiniComponent',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 50px 0 0;
+  color: red;
+}
+p {
+  color: #42b983;
+}
+</style>
